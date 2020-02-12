fae5cef47380a20ee2dff887a1fa5f4deb13b6a9
Jose Antonio Guerra <jaguerra@tuenti.com>
2020-02-12 14:45:21 +0100

    Add configurable first CR on env, added examples

--- src/main/kotlin/com/github/jochettino/fisojo/config/EnvironmentConfigHandlerImpl.kt
+++ src/main/kotlin/com/github/jochettino/fisojo/config/EnvironmentConfigHandlerImpl.kt
@@ -20,7 +20,7 @@ class EnvironmentConfigHandlerImpl: ConfigHandler {
             feauth = System.getenv(FISHEYE_FEAUTH) ?: "",
             projectId = System.getenv(FISHEYE_PROJECT_ID) ?: "",
             baseServerUrl = System.getenv(FISHEYE_BASE_SERVER_URL) ?: "",
-            lastCrNumber = 0,
+            lastCrNumber = System.getenv(FISHEYE_LAST_CR_NUMBER)?.toInt() ?: 0,
             pollingFrequency = System.getenv(FISHEYE_POLLING_FREQUENCY)?.toLong() ?: ConfigDefaults.POLLING_FREQUENCY,
             secondsToLookIntoThePast = System.getenv(FISHEYE_SECONDS_TO_LOOK_INTO_THE_PAST)?.toLong()
                     ?: ConfigDefaults.SECONDS_TO_LOOK_INTO_THE_PAST
@@ -43,6 +43,7 @@ class EnvironmentConfigHandlerImpl: ConfigHandler {
         private const val FISHEYE_FEAUTH = "FISHEYE_FEAUTH"
         private const val FISHEYE_PROJECT_ID = "FISHEYE_PROJECT_ID"
         private const val FISHEYE_BASE_SERVER_URL = "FISHEYE_BASE_SERVER_URL"
+        private const val FISHEYE_LAST_CR_NUMBER = "FISHEYE_LAST_CR_NUMBER"
         private const val FISHEYE_POLLING_FREQUENCY = "FISHEYE_POLLING_FREQUENCY"
         private const val FISHEYE_SECONDS_TO_LOOK_INTO_THE_PAST = "FISHEYE_SECONDS_TO_LOOK_INTO_THE_PAST"
 
